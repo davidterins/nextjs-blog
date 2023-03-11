f0f9ba434e58cd0ccd71a4a1515674b6c4fdeeb7
David Terins <david.terins@icloud.com>
2023-03-11 10:51:40 +0100

    between naps...

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,6 +2,7 @@ import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.scss";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -26,11 +27,10 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      <Date dateString={postData.date} />
+      <h1 className="utilStyles.headingXl">{postData.title}</h1>
+      <div className="utilStyles.lightText">
+        <Date dateString={postData.date} />
+      </div>
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
