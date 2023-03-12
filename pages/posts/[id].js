b701fa10da382b7893789f22a71ccf0819d462c4
DTee <david.terins@telia.com>
2023-03-12 08:17:42 +0100

    next up - deploy

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -27,11 +27,13 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <h1 className="utilStyles.headingXl">{postData.title}</h1>
-      <div className="utilStyles.lightText">
-        <Date dateString={postData.date} />
-      </div>
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
